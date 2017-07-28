0343545ae78168f1c0aa98b420ae9e85a027f017
Yantzzz <ty2735600@gmail.com>
2017-07-28 16:04:53 -0700

    Discount calculator

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiscountCalculator
+//
+//  Created by Yan Tian on 7/25/17.
+//  Copyright © 2017 Yan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,59 @@
+//
+//  ViewController.m
+//  DiscountCalculator
+//
+//  Created by Yan Tian on 7/25/17.
+//  Copyright © 2017 Yan Tian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *inputTextField;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *controlPad;
+
+@property (weak, nonatomic) IBOutlet UILabel *savedAmount;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalAmount;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)didTapScreen:(id)sender {
+    [self updateValue];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)didTapControlPad:(id)sender {
+    [self updateValue];
+}
+
+- (void)updateValue
+{
+    [self.view endEditing:YES];
+    
+    CGFloat billAmount = [self.inputTextField.text floatValue];
+    
+    NSArray *discountRate = @[@(0.9),@(0.8),@(0.7)];
+    
+    CGFloat savedPrice = billAmount * (1 - [discountRate[[self.controlPad selectedSegmentIndex]] floatValue]);
+    CGFloat totalPrice = billAmount * [discountRate[[self.controlPad selectedSegmentIndex]] floatValue];
+    
+    self.savedAmount.text = [NSString stringWithFormat:@"$%10.2f",savedPrice];
+    self.totalAmount.text = [NSString stringWithFormat:@"$%10.2f",totalPrice];
+}
+
+
+@end
